3e57fa0927485d8b8d1e486fff448e3123fceefa
Pas,Thom T.A.H.M. van de <thom.vandepas@student.fontys.nl>
2016-12-09 10:59:50 +0100

    Podfile updated and FoodtruckAPI created

---
+++ Foodtruck/Foodtruck/FoodtruckAPI.swift
@@ -0,0 +1,14 @@
+//
+//  FoodtruckAPI.swift
+//  Foodtruck
+//
+//  Created by Fhict on 09/12/16.
+//  Copyright © 2016 Fhict. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class FoodtruckAPI {
+    
+}
