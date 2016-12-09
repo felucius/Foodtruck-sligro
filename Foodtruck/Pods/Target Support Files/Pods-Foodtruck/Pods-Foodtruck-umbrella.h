6153ed5a987bacf633e6deec33bc0571d96b40b3
Bakker,Tom T.A.C <tac.bakker@student.fontys.nl>
2016-12-09 09:42:30 +0100

    Installed AlamoFire for networking

---
+++ Foodtruck/Pods/Target Support Files/Pods-Foodtruck/Pods-Foodtruck-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FoodtruckVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FoodtruckVersionString[];
+
---
+++ Foodtruck/Pods/Target Support Files/Pods-FoodtruckUITests/Pods-FoodtruckUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodtruckUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodtruckUITests
+@end
